8f3aaebc8c6fcec29116c3bdae36d05d72e00af9
Jenn Simpson <jsimpson@yourtaxsource.com>
2026-08-22 23:25:52 +0000

    debug: move debug-env to /api/debug-env

--- test-env.ts
+++
@@ -1,4 +0,0 @@
-import { validatedEnv } from "./src/lib/env";
-console.log("OPENAI_API_KEY in process.env:", !!process.env.OPENAI_API_KEY);
-console.log("TAVILY_API_KEY in process.env:", !!process.env.TAVILY_API_KEY);
-console.log("OPENAI_API_KEY in validatedEnv:", !!(validatedEnv as any).OPENAI_API_KEY);
